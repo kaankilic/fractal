090174fdee6817835dd5d0d40690db8c91ec6c20
Kaan <kiliclarinkaan@gmail.com>
2022-10-23 22:53:47 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
+import model from './model';
 export default {
     install(Vue, options) {
-        Vue.mixin({
-            mounted() {
-                console.log('Mounted!');
-            }
-        });
+        Vue.prototype.$fractal = model;
         Vue.VERSION = 'v2.0.3';
     }
 }
--- src/model.js
+++ src/model.js
@@ -1,55 +1,12 @@
-import Vue from 'vue';
-const mapper = (schema, data) => {
-    if (Array.isArray(data)) {
-        let entity = data.map(o => {
-            let obj = Object.assign(...Object.entries(schema).map(([k, v]) => {
-                if (typeof v === 'string') {
-                    return ({
-                        [k]: o[v]
-                    })
-                }
-                if (typeof v === 'object') {
-                    let inner = o[k].map(i => {
-                        return Object.assign(...Object.entries(schema[k]).map(([s,
-                            j
-                        ]) => ({
-                            [s]: i[j]
-                        })));
-                    });
-                    return ({
-                        [k]: inner
-                    });
-                }
-            }))
-            return Vue.observable(obj);
-        });
-        return entity;
-    } else {
-        let obj = Object.assign(...Object.entries(schema).map(([k, v]) => {
-            if (typeof v === 'string') {
-                return ({
-                    [k]: data[v]
-                })
-            }
-            if (typeof v === 'object') {
-                let inner = Object.assign(...Object.entries(schema[k]).map(([s,
-                    j
-                ]) => {
-                    if (Array.isArray(data[k][s]) && typeof j === 'object') {
-                        const recursive_mapper = mapper(j, data[k][s]);
-                        return { [s] : recursive_mapper }
-                    } else {
-                        return {
-                            [s]: data[k][j]
-                        }
-                    }
-                }));
-                return ({
-                    [k]: inner
-                });
-            }
-        }))
-        return Vue.observable(obj);
+import Collection from "./collection";
+
+export default{
+    mounted(){
+        console.log("fractal mounted");
+    },
+    methods:{
+        collection(data, transformer, resourceKey){
+            return new Collection(data, transformer, resourceKey)
+        }
     }
 }
-export default mapper;
